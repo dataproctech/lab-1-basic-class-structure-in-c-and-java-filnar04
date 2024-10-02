590e2d685459861fa4c15707f7247f8406fce017
filnar04 <zfsd1233@gmail.com>
2024-10-02 14:36:19 +0200

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,15 +1,15 @@
 public class Car {
 
-    private String marka, model;
+    private String make, model;
     private int year;
 
     public Car(String marka, String model, int year) {
-        this.marka = marka;
+        this.make = marka;
         this.model = model;
         this.year = year;
     }
     public void displayInfo() {
-        System.out.printf("model: %s %s\nrok produkcji: %d", marka, model, year);
+        System.out.printf("model: %s %s\nrok produkcji: %d", make, model, year);
     }
     public static void main(String args[]) {
         Car car1 = new Car("test", "test", 2000);
