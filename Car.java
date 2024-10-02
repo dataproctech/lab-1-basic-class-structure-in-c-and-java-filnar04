dcae65b9d938c7a2d8f6f2a97b945624743933c6
filnar04 <zfsd1233@gmail.com>
2024-10-02 14:32:37 +0200

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,16 +1,18 @@
 public class Car {
-    // Attributes
-    
-    // Constructor
-    
-    // Method to display car information
-    
-    }
 
-    public static void main(String[] args) {
-        // Create a Car object
-        
-        // Display car details
-        
+    private String marka, model;
+    private int year;
+
+    public Car(String marka, String model, int year) {
+        this.marka = marka;
+        this.model = model;
+        this.year = year;
+    }
+    public void displayInfo() {
+        System.out.printf("model: %s %s\nrok produkcji: %d", marka, model, year);
+    }
+    public static void main(String args[]) {
+        Car car1 = new Car("test", "test", 2000);
+        car1.displayInfo();
     }
 }
