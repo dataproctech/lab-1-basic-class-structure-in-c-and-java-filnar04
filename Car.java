65afeac3a6816bfef6b8865e86f564ebb97bc469
filnar04 <zfsd1233@gmail.com>
2024-10-14 20:13:11 +0200

    Update Car.java
    - added currentYear attribute
    - added getCarAge and isSameCar methods
    - added comments

--- Car.java
+++ Car.java
@@ -1,18 +1,35 @@
 public class Car {
 
     private String make, model;
-    private int year;
+    private int year, currentYear;
 
-    public Car(String marka, String model, int year) {
-        this.make = marka;
+    public Car(String make, String model, int year) {
+        this.make = make;
         this.model = model;
         this.year = year;
+        this.currentYear = 2024;
     }
+    //print information about car
     public void displayInfo() {
-        System.out.printf("model: %s %s\nrok produkcji: %d", make, model, year);
+        System.out.printf("%s %s %d", make, model, year);
+    }
+    //calculate and display car age
+    public void getCarAge() {
+        System.out.printf("\ncar age: %d", currentYear - year);
+    }
+    //check if make, model and year are equal
+    public boolean isSameCar(Car otherCar) {
+        if (make == otherCar.make && model == otherCar.model && year == otherCar.year) return true;
+        return false;
     }
     public static void main(String args[]) {
-        Car car1 = new Car("test", "test", 2000);
-        car1.displayInfo();
+        //create test objects
+        Car car = new Car("test", "test", 2000);
+        Car anotherCar = new Car("test", "test", 1999);
+        //test methods
+        car.displayInfo();
+        car.getCarAge();
+        if (car.isSameCar(anotherCar)) System.out.println("\nThe same");
+        else System.out.println("\nNot the same");
     }
 }
