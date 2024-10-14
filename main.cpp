ce4ceb3a462d95aa0ee5316f934cf9b6da099ae2
filnar04 <zfsd1233@gmail.com>
2024-10-14 12:54:37 +0200

    Update main.cpp
    - added currentYear attribute
    - added getCarAge() and isSameCar() methods
    - added comments

--- main.cpp
+++ main.cpp
@@ -1,21 +1,36 @@
 #include<iostream>
+#define CURRENT_YEAR 2024
 
 class Car {
     private:
     std::string make, model;
-    int year;
+    int year, currentYear;
     public:
+    //print information about car
     void displayInfo() {
         std::cout << "model: " << make << ' ' << model << "\nrok produkcji: " << year;
+    }
+    //calculate and display car age
+    void getCarAge() {
+        std::cout << "\nCar age: << currentYear - year; 
+    }
+    //check if make, model and year are equal
+    bool isSameCar(Car otherCar) {
+        return (make == otherCar.make && model == otherCar.model && year == otherCar.year);
     }
     Car(std::string make, std::string model, int year) {
         this->make=make;
         this->model=model;
         this->year=year;
+        this->currentYear=CURRENT_YEAR;
     }
 };
 
 int main() {
-    Car car1("test", "test", 2000);
-    car1.displayInfo();
+    //create test objects
+    Car car("test", "test", 2000), anotherCar("test", "test", 1999);    
+    //test methods
+    car.displayInfo();
+    car.getCarAge();
+    std::cout << car.isSameCar(anotherCar);
 }
